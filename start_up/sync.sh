e63a7bce478992fc7063b7aa6cf4bbac7b66da1e
GH0STAV0 <you@example.com>
2022-06-10 22:26:18 +0100

    my connecto python

--- start_up/sync.sh
+++ start_up/sync.sh
@@ -3,16 +3,16 @@
 echo "-------------------------> START SYCHRONIZNG <-------------------------------------"
 supervisorctl status
 whoami
-mkdir -p /var/www/html/service_online
+su www-data mkdir -p /var/www/html/service_online
 pwd
 /usr/bin/mega-quit
 sleep 2
 # ls
-bash -c '/usr/bin/mega-login "ecruakod73@r0b-in.nl.eu.org" "R_cA4s_A5aMK8Es"'
-bash -c '/usr/bin/mega-mount'
+bash -c 'su www-data /usr/bin/mega-login "ecruakod73@r0b-in.nl.eu.org" "R_cA4s_A5aMK8Es"'
+bash -c 'su www-data /usr/bin/mega-mount'
 #mega-get /sihati-lab/real/ /var/www/html/
 # cp real/*.pdf /var/www/html/service_online/
-bash -c '/usr/bin/mega-sync /var/www/html/service_online /sihati-lab/real'
+bash -c 'su www-data /usr/bin/mega-sync /var/www/html/service_online /sihati-lab/real'
 
 echo "sleep for 5 seconds "
 sleep 5
