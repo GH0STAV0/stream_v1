7c5c038d33f65e623f2651c29a977d2bdd30c824
GH0STAV0 <you@example.com>
2022-06-10 18:19:28 +0100

    my connecto python

--- start_up/sync.sh
+++ start_up/sync.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -x
+# set -x
 echo "-------------------------> START SYCHRONIZNG <-------------------------------------"
 
 whoami
 mkdir /var/www/html/service_online
-
+pwd
 # ls
-exec mega-login ecruakod73@r0b-in.nl.eu.org R_cA4s_A5aMK8Es
+exec mega-login "ecruakod73@r0b-in.nl.eu.org" "R_cA4s_A5aMK8Es"
 exec mega-mount
 #mega-get /sihati-lab/real/ /var/www/html/
 # cp real/*.pdf /var/www/html/service_online/
