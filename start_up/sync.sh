63cf398db603fb6751e339b69df3b1690f1d89f8
GH0STAV0 <you@example.com>
2022-06-10 20:31:08 +0100

    my connecto python

--- start_up/sync.sh
+++ start_up/sync.sh
@@ -5,6 +5,8 @@ supervisorctl status
 whoami
 mkdir -p /var/www/html/service_online
 pwd
+/usr/bin/mega-quit
+sleep 2
 # ls
 /usr/bin/mega-login "ecruakod73@r0b-in.nl.eu.org" "R_cA4s_A5aMK8Es"
 /usr/bin/mega-mount
