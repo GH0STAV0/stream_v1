1cf2382a10cd80ca18ec6236a450a15f0b802561
GH0STAV0 <you@example.com>
2022-06-10 20:25:30 +0100

    my connecto python

--- start_up/sync.sh
+++ start_up/sync.sh
@@ -6,11 +6,11 @@ whoami
 mkdir -p /var/www/html/service_online
 pwd
 # ls
-mega-login "ecruakod73@r0b-in.nl.eu.org" "R_cA4s_A5aMK8Es"
-mega-mount
+/usr/bin/mega-login "ecruakod73@r0b-in.nl.eu.org" "R_cA4s_A5aMK8Es"
+/usr/bin/mega-mount
 #mega-get /sihati-lab/real/ /var/www/html/
 # cp real/*.pdf /var/www/html/service_online/
-mega-sync /var/www/html/service_online /sihati-lab/real
+/usr/bin/mega-sync /var/www/html/service_online /sihati-lab/real
 
 echo "sleep for 5 seconds "
 sleep 5
